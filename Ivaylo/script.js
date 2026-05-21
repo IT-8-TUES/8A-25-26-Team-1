271ea17c5f8d0932e4b6ce7ded6dda23c72a1d90
ivaylo <ivaylo.i.petrov.2025@elsys-bg.org>
2026-05-21 19:20:21 +0300

    yjbnkf

---
+++ Ivaylo/script.js
@@ -0,0 +1,18 @@
+const info1=document.getElementById("info1");
+const info2=document.getElementById("info2");
+const info3=document.getElementById("info3");
+
+info1.style.display='none';
+let s=0;
+function showr(a){
+    if(a===1) info1.style.display='block';
+
+}
+function hider(a){
+    if(a===1) info1.style.display='none';
+}
+function show(a){
+    s++;
+    if(s%2===1) showr(a);
+    else hider(a);
+}
